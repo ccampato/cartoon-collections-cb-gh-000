5ac4c83f62f4b043a08b22e2f40d3afd0ed7a950
Caio Sanches <ccampato@gmail.com>
2018-04-20 23:11:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(a)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(a)
+  a.map { |i| "#{i.capitalize}!" }
 end
 
 def long_planeteer_calls# code an argument here
