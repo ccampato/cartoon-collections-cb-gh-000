03aca6560278dddeef106bf44e5a97a5ad84662d
Caio Sanches <ccampato@gmail.com>
2018-04-20 23:23:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,9 @@ def long_planeteer_calls(a)
   a.any? { |calls| calls.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(a)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  a.find { |chesse| cheese == cheese_types.include?(cheese)  }
 end
